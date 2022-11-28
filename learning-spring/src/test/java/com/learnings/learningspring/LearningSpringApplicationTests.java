a3071f7a43b51cdf1a62ef98f2c13ed646ffeac3
Pratyush Kodavanti <vkodavanti@athenahealth.com>
2022-11-27 20:45:24 -0500

    Embed Databse with SpringBoot

---
+++ learning-spring/src/test/java/com/learnings/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
